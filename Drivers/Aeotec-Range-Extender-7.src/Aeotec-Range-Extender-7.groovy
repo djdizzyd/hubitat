31148620a66e7fb035ffff8b4cc1629d1fbefd86
djdizzyd <bcopeland@tanmarcompanies.com>
2020-04-10 06:50:45 -0500

    aeotec range extender 7

---
+++ Drivers/Aeotec-Range-Extender-7.src/Aeotec-Range-Extender-7.groovy
@@ -0,0 +1,173 @@
+import groovy.transform.Field
+
+metadata {
+
+    definition (name: "Aeotec Range Extender 7", namespace: "djdizzyd", author: "Bryan Copeland") {
+
+        capability "Actuator"
+        capability "Configuration"
+        capability "Light"
+        capability "Switch"
+
+        fingerprint mfr:"0371", prod:"0104", deviceId:"00BD", deviceJoinName: "Aeotec Range Extender 7"
+
+    }
+    preferences {
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+        input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
+    }
+}
+
+@Field static Map CMD_CLASS_VERS=[0x33:2,0x26:2,0x86:2,0x70:1]
+
+void on() {
+    List<hubitat.zwave.Command> cmds=[]
+    cmds.add(zwave.indicatorV1.indicatorSet(value:0xFF))
+    cmds.add(zwave.indicatorV1.indicatorGet())
+    sendToDevice(cmds)
+}
+
+void off() {
+    List<hubitat.zwave.Command> cmds=[]
+    cmds.add(zwave.indicatorV1.indicatorSet(value:0x00))
+    cmds.add(zwave.indicatorV1.indicatorGet())
+    sendToDevice(cmds)
+}
+
+void logsOff(){
+    log.warn "debug logging disabled..."
+    device.updateSetting("logEnable",[value:"false",type:"bool"])
+}
+
+void configure() {
+    runIn(5,pollDeviceData)
+}
+
+void updated() {
+    log.info "updated..."
+    log.warn "debug logging is: ${logEnable == true}"
+    unschedule()
+    if (logEnable) runIn(1800,logsOff)
+}
+
+void pollDeviceData() {
+    List<hubitat.zwave.Command> cmds = []
+    cmds.add(zwave.versionV2.versionGet())
+    cmds.add(zwave.manufacturerSpecificV2.deviceSpecificGet(deviceIdType: 1))
+    cmds.addAll(processAssociations())
+    cmds.add(zwave.indicatorV1.indicatorGet())
+    sendToDevice(cmds)
+}
+
+void zwaveEvent(hubitat.zwave.commands.securityv1.SecurityMessageEncapsulation cmd) {
+    hubitat.zwave.Command encapsulatedCommand = cmd.encapsulatedCommand(CMD_CLASS_VERS)
+    if (encapsulatedCommand) {
+        zwaveEvent(encapsulatedCommand)
+    }
+}
+
+void parse(String description) {
+    if (logEnable) log.debug "parse:${description}"
+    hubitat.zwave.Command cmd = zwave.parse(description, CMD_CLASS_VERS)
+    if (cmd) {
+        zwaveEvent(cmd)
+    }
+}
+
+void zwaveEvent(hubitat.zwave.commands.supervisionv1.SupervisionGet cmd) {
+    if (logEnable) log.debug "Supervision get: ${cmd}"
+    hubitat.zwave.Command encapsulatedCommand = cmd.encapsulatedCommand(CMD_CLASS_VERS)
+    if (encapsulatedCommand) {
+        zwaveEvent(encapsulatedCommand)
+    }
+    sendToDevice(new hubitat.zwave.commands.supervisionv1.SupervisionReport(sessionID: cmd.sessionID, reserved: 0, moreStatusUpdates: false, status: 0xFF, duration: 0))
+}
+
+void zwaveEvent(hubitat.zwave.commands.manufacturerspecificv2.DeviceSpecificReport cmd) {
+    if (logEnable) log.debug "Device Specific Report: ${cmd}"
+    switch (cmd.deviceIdType) {
+        case 1:
+            // serial number
+            def serialNumber=""
+            if (cmd.deviceIdDataFormat==1) {
+                cmd.deviceIdData.each { serialNumber += hubitat.helper.HexUtils.integerToHexString(it & 0xff,1).padLeft(2, '0')}
+            } else {
+                cmd.deviceIdData.each { serialNumber += (char) it }
+            }
+            device.updateDataValue("serialNumber", serialNumber)
+            break
+    }
+}
+
+void zwaveEvent(hubitat.zwave.commands.versionv2.VersionReport cmd) {
+    if (logEnable) log.debug "version3 report: ${cmd}"
+    device.updateDataValue("firmwareVersion", "${cmd.firmware0Version}.${cmd.firmware0SubVersion}")
+    device.updateDataValue("protocolVersion", "${cmd.zWaveProtocolVersion}.${cmd.zWaveProtocolSubVersion}")
+    device.updateDataValue("hardwareVersion", "${cmd.hardwareVersion}")
+}
+
+void sendToDevice(List<hubitat.zwave.Command> cmds) {
+    sendHubCommand(new hubitat.device.HubMultiAction(commands(cmds), hubitat.device.Protocol.ZWAVE))
+}
+
+void sendToDevice(hubitat.zwave.Command cmd) {
+    sendHubCommand(new hubitat.device.HubAction(secureCommand(cmd), hubitat.device.Protocol.ZWAVE))
+}
+
+void sendToDevice(String cmd) {
+    sendHubCommand(new hubitat.device.HubAction(secureCommand(cmd), hubitat.device.Protocol.ZWAVE))
+}
+
+List<String> commands(List<hubitat.zwave.Command> cmds, Long delay=200) {
+    return delayBetween(cmds.collect{ secureCommand(it) }, delay)
+}
+
+String secureCommand(hubitat.zwave.Command cmd) {
+    secureCommand(cmd.format())
+}
+
+String secureCommand(String cmd) {
+    String encap=""
+    if (getDataValue("zwaveSecurePairingComplete") != "true") {
+        return cmd
+    } else {
+        encap = "988100"
+    }
+    return "${encap}${cmd}"
+}
+
+void zwaveEvent(hubitat.zwave.Command cmd) {
+    if (logEnable) log.debug "skip:${cmd}"
+}
+
+List<hubitat.zwave.Command> setDefaultAssociation() {
+    List<hubitat.zwave.Command> cmds=[]
+    cmds.add(zwave.associationV2.associationSet(groupingIdentifier: 1, nodeId: zwaveHubNodeId))
+    cmds.add(zwave.associationV2.associationGet(groupingIdentifier: 1))
+    return cmds
+}
+
+List<hubitat.zwave.Command> processAssociations(){
+    List<hubitat.zwave.Command> cmds = []
+    cmds.addAll(setDefaultAssociation())
+    return cmds
+}
+
+void zwaveEvent(hubitat.zwave.commands.associationv2.AssociationReport cmd) {
+    if (logEnable) log.debug "${device.label?device.label:device.name}: ${cmd}"
+    List<String> temp = []
+    if (cmd.nodeId != []) {
+        cmd.nodeId.each {
+            temp.add(it.toString().format( '%02x', it.toInteger() ).toUpperCase())
+        }
+    }
+    updateDataValue("zwaveAssociationG${cmd.groupingIdentifier}", "$temp")
+}
+
+void zwaveEvent(hubitat.zwave.commands.indicatorv3.IndicatorReport cmd) {
+    if (cmd.value>0) {
+        sendEvent(name: "switch", value: "on", isStateChange: true)
+    } else {
+        sendEvent(name: "switch", value: "off", isStateChange: true)
+    }
+}
