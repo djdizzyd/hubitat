e8de1cc5b66ba177923f7b4c9d2e5be12804dd97
Bryan Copeland <bryan.copeland@me.com>
2020-07-19 15:40:50 -0500

    hsv100to360

--- Apps/hsv100to360.groovy
+++ Apps/hsv100to360.groovy
@@ -17,14 +17,23 @@ preferences {
 def mainPage() {
     dynamicPage(name: "mainPage", title: "Hubitat HSV100 Colors", stall: true, uninstall: true) {
         section {
-            String table = "<table border=1><tr><th>Hue<br/>360</th><th>Hue<br/>100</th><th>Color<br/>Hex</th></tr>"
-            for (int i in 0..100) {
-                List hsv100 = [i,100,100]
+            String table = "<table border=1><tr><th>Hue<br/>360</th><th>Hue<br/>100</th><th>Color<br/>Hex</th>"
+            for (int i in (95..0).step(5)) {
+                table += "<th>sat<br/>${i}</th>"
+            }
+            table +="</tr>"
+            for (int h in 0..100) {
+                List hsv100 = [h,100,100]
                 List rgbColor = hubitat.helper.ColorUtils.hsvToRGB(hsv100)
                 List hsv360 = hubitat.helper.ColorUtils.rgbToHSV360(rgbColor)
                 String rgbHex = hubitat.helper.ColorUtils.rgbToHEX(rgbColor)
-                if (i==100) hsv360[0]=360
-                table += "<tr><td>${Math.round(hsv360[0])}</td><td>${i}</td><td bgcolor='${rgbHex}'>${rgbHex}</td></tr>"
+                if (h==100) hsv360[0]=360
+                table += "<tr><td>${Math.round(hsv360[0])}</td><td>${h}</td><td bgcolor='${rgbHex}'>${rgbHex}</td>"
+                for (int s in (95..0).step(5)) {
+                    String rgbColorSat = hubitat.helper.ColorUtils.rgbToHEX(hubitat.helper.ColorUtils.hsvToRGB([h,s,100]))
+                    table += "<td bgcolor='${rgbColorSat}'></td>"
+                }
+                table += "</tr>"
             }
             table += "</table>"
             paragraph table
